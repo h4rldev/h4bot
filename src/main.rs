4c0c5c63d725bb7694bd2897956d2802d4f42728
h4rl <h4rldev@proton.me>
2023-08-10 04:49:53 +0200

    add !ping

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,41 @@ use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
+use std::time::Instant;
 
 struct Bot;
+
+async fn measure_latency(ctx: &Context, msg: &Message) {
+    let start_time = Instant::now();
+    let response = msg.channel_id.say(&ctx.http, "Pong!").await;
+    let end_time = Instant::now();
+
+    if let Ok(mut response) = response {
+        let latency = end_time.duration_since(start_time).as_micros();
+        response
+            .edit(&ctx.http, |m| {
+                m.content(format!("Pong! Latency: {}µs", latency))
+                    .allowed_mentions(|f| f.empty_parse());
+                m
+            })
+            .await
+            .unwrap();
+    }
+}
+
+
 
 #[async_trait]
 impl EventHandler for Bot {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
+        match msg.content.as_str() {
+            "!hello" => if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
                 error!("Error sending message: {:?}", e);
-            }
+            },
+            "!ping" => measure_latency(&ctx, &msg).await,
+            &_ => {}
         }
     }
-
     async fn ready(&self, _: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
     }
